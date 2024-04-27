08c8255dbc192a4ccee47aa7ef2d2bfcf0feba88
Dmitrii Bubnov <bubnov.dmitrii@mail.ru>
2024-04-27 11:59:42 +0300

    Added comments to final problems

--- 3rd_Sprint/BrockenSearch.cpp
+++ 3rd_Sprint/BrockenSearch.cpp
@@ -1,5 +1,38 @@
-//https://contest.yandex.ru/contest/23815/run-report/112435977/
-//#include "solution.h"
+//https://contest.yandex.ru/contest/23815/run-report/113078991/
+/*
+-- ПРИНЦИП РАБОТЫ --
+Решение заключается в том, чтобы с помощью функции GetPartitionBorder найти точку стыка между двумя отсортированными 
+частями массива. Функция использует бинарный поиск и рекурсивно ищет элемент такой, чтобы он был меньше и предыдущего, и следующего 
+за ним элементов. Найдя такой элемент, функция возвращает итератор на него. Предварительно GetPartitionBorder сравнивает
+первый элемент массива с последним, в случае, если последний больше первого, возвращает итератор на элемент, следующий
+за последним. Эта ситуация означает, что кольцевой буфер был разорван корректно и упорядоченность элементов сохранилась.
+Затем для поиска индекса элемента выполняется бинарный поиск в двух отсортированных половинах массива, либо во всем массиве сразу,
+если он сохранил упорядоченность. Из полученного с помощью std::lower_bound() итератора и begin() расчитывается индекс.
+Если элемент не был найден, GetValueIndex возвращает -1.
+
+
+-- ДОКАЗАТЕЛЬСТВО КОРРЕКТНОСТИ --
+Разрыв отсортированного по неубыванию массива на кольцевом буфере может привести только к двум исходам: массив останется упорядоченным,
+либо будет содержать два подмассива, все элементы первого из которых будут не меньше всех элементов второго. Первая ситуация проверяется 
+путем сравнения первого и последнего элементов массива. Если упорядоченность сохранена, то поиск искомого элемента можно вести во всем массиве целиком.
+В противном случае, нужно найти точку сочленения двух упорядоченных половин. Первый элемент второго массива, содержащего меньшие элементы должен быть
+больше следующего за ним и меньше предыдущего. При этом, при соблюдении условия задачи, такой элемент будет единственным. Исходя из этого свойства, 
+граница ищется бинарным поиском. Если на очередной итерации поиска целевой элемент не был найден, нужно решить, с какой стороны
+от текущего элемента искать. Для этого текущий элемент сравнивается с первым элементом. Если текущий больше первого, то искать нужно справа и наоборот.
+Затем, когда граница найдена, выполняется поиск искомого элемента в двух упорядоченных половинах.
+
+
+-- ВРЕМЕННАЯ СЛОЖНОСТЬ --
+Поиск границы разрыва имеет сложность, не более, чем log2(N), где N - длина данного массива. Поиск искомого элемента имеет сложность, не более, чем
+2log2(N), так как сначала проверяется наличие элемента в подмассиве за log2(n), а затем за log2(n) выполняется поиск итератора, указывающего на него. При этом n <= N. 
+Таким образом общаая сложность алгоритма не более 3log2(N).
+
+-- ПРОСТРАНСТВЕННАЯ СЛОЖНОСТЬ --
+Алгоритм использует дополнительную память только за тем, чтобы хранить один итератор в функциях GetPartitionBorder и broken_search. Таким образом, пространственная сложность - O(1).
+*/
+
+
+#include "solution.h"
 #include <vector>
 #include <iostream>
 #include <algorithm>
@@ -10,26 +43,27 @@ Iterator GetPartitionBorder(Iterator first, Iterator last, ValueType first_value
 	if (*first < *std::prev(last)){
 		return last;
 	}
+
 	Iterator middle = first + (last - first) / 2;
 	if ((*middle < *std::prev(middle) && std::next(middle) == last) || (*middle < *std::prev(middle) && *middle < *std::next(middle))){
 		return middle;
 	}
-	if (*middle > *std::prev(middle) && *middle > *std::next(middle)){
+	else if (*middle > *std::prev(middle) && *middle > *std::next(middle)){
 		return std::next(middle);
 	}
-	if (*middle > *std::prev(middle) && *middle < *std::next(middle) && *middle > first_value){
+	else if (*middle > *std::prev(middle) && *middle < *std::next(middle) && *middle > first_value){
 		return GetPartitionBorder(middle, last, first_value);
 	}
-	if (*middle > *std::prev(middle) && *middle < *std::next(middle) && *middle < first_value){
+	else if (*middle > *std::prev(middle) && *middle < *std::next(middle) && *middle < first_value){
 		return GetPartitionBorder(first, middle, first_value);
 	}
-	return last;
-	
+	else {
+		return last;
+	}	
 }
 
 template <typename Iterator, typename ValueType>
 int GetValueIndex(Iterator first, Iterator last, Iterator border, ValueType value){
-	int result = 0;
 	if (border == last){
 		return (!std::binary_search(first, last, value)) ? -1 : std::lower_bound(first, last, value) - first;
  	}
